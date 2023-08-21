fb96ada068cbea211e2748686395a0cd0b8df2fa
Toby Churches [SSW] <61444937+TobyChurches@users.noreply.github.com>
2023-08-21 13:32:57 +1000

    Bootstrap Icons added, search use case can now be navigated with fake data

--- WebStore/Controllers/SearchController.cs
+++ WebStore/Controllers/SearchController.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly StoreDbContext _context;
+
+        public SearchController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult ItemDetails()
         {
             return View();
         }
 
-        public IActionResult SearchResults()
+        [HttpPost]
+        public ActionResult SearchResults(string searchQuery)
         {
+            // Handle the search logic here
+
+            ViewBag.SearchQuery = searchQuery;
             return View();
         }
     }
